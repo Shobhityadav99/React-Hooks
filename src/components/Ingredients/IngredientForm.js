27d1d01e5cc1d45344a6a9896479b52031f38222
Shobhit Yadav <shobhit999000@gmail.com>
2021-01-29 15:34:14 +0530

    Array destructuring

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({
+  const [inputState,setInputState] = useState({
     title: '',
     amount: ''
   })
@@ -22,16 +22,24 @@ const IngredientForm = React.memo(props => {
             <input 
             type="text" 
             id="title" 
-            value={inputState[0].title} 
-            onChange={event => inputState[1]({title: event.target.value, amount: inputState[0].amount})}/>
+            value={inputState.title} 
+            onChange={event => {
+              const newTitle= event.target.value;
+              setInputState(prevState => ({
+                title: newTitle,
+                amount: prevState.amount}))}}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             <input 
             type="number" 
             id="amount" 
-            value={inputState[0].amount}
-            onChange={event => inputState[1]({title: inputState[0].title,amount: event.target.value})} />
+            value={inputState.amount}
+            onChange={event => {
+              const newAmount= event.target.value;
+              setInputState(prevState => ({
+                title: prevState.title,
+                amount: newAmount}))}} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
